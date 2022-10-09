6d1866f4aac1ac9b973346b43120fb331c20b6a8
Ilya <80617082+imatyushov@users.noreply.github.com>
2022-10-09 16:57:09 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -59,7 +59,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    }, [])
+    }, [addedItems])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
